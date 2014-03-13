fb2efb98123407c556947a0a2b2b75f39ea61f83
Ge Yang <yangge1987@gmail.com>
2014-03-13 16:50:39 -0500

    added comment in the test scrpt, and add all test back.

--- liveplot_test.py
+++ liveplot_test.py
@@ -9,32 +9,33 @@ for i in range(100):
    c.plot_y('scrolling sine', arr)
    time.sleep(.05)
 
-# xs = np.linspace(-1, 1, 100)
-# for mu in xs:
-#     ys = np.exp((-(xs - mu)**2)/.1)
-#     c.plot_xy('travelling packet', xs, ys)
-#     time.sleep(.05)
-#
-# for val in np.exp(np.linspace(0, 6, 100)):
-#     c.append_y('appending exp', val)
-#     time.sleep(.05)
-#
-# for i in range(100):
-#    ts = np.linspace(0, 20, 300) + i/20.
-#    xs = ts**2 * np.sin(ts)
-#    ys = ts**2 * np.cos(ts)
-#    c.plot_xy('rotating spiral', xs, ys)
-#    time.sleep(.05)
-#
-# c.clear('spiral out')
-# for x, y in zip(xs, ys):
-#     c.append_xy('spiral out', x, y)
-#     time.sleep(.05)
-#
-# xs, ys = np.mgrid[-100:100, -100:100]/20.
-# rs = np.sqrt(xs**2 + ys**2)
-# for i in range(100):
-#     c.plot_z('sinc', np.sinc(rs + i/20.), extent=((-5, 5), (-10, 10)))
-#     time.sleep(.05)
+xs = np.linspace(-1, 1, 100)
+for mu in xs:
+    ys = np.exp((-(xs - mu)**2)/.1)
+    c.plot_xy('travelling packet', xs, ys)
+    time.sleep(.05)
 
+for val in np.exp(np.linspace(0, 6, 100)):
+    c.append_y('appending exp', val)
+    time.sleep(.05)
+
+for i in range(100):
+   ts = np.linspace(0, 20, 300) + i/20.
+   xs = ts**2 * np.sin(ts)
+   ys = ts**2 * np.cos(ts)
+   c.plot_xy('rotating spiral', xs, ys)
+   time.sleep(.05)
+
+c.clear('spiral out')
+for x, y in zip(xs, ys):
+    c.append_xy('spiral out', x, y)
+    time.sleep(.05)
+
+xs, ys = np.mgrid[-100:100, -100:100]/20.
+rs = np.sqrt(xs**2 + ys**2)
+for i in range(100):
+    c.plot_z('sinc', np.sinc(rs + i/20.), extent=((-5, 5), (-10, 10)))
+    time.sleep(.05)
+
+#sending clear() to the plotter clears all the open windows and removes them from the list.
 c.clear()
